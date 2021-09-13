b164c46c08d4fc4288fb5f41fc529928288a60a1
Sunny Yoo <isunyoo@gmail.com>
2021-09-13 20:49:22 +0800

    Updated for changes

--- README.txt
+++ README.txt
@@ -2,11 +2,7 @@ NFT Python
 https://www.freecodecamp.org/news/how-to-make-an-nft-and-render-on-opensea-marketplace/
 https://betterprogramming.pub/how-to-create-nfts-with-solidity-4fa1398eb70a
 https://opensea.io
-
-https://ipfs.io/ipfs/QmX2T6xvEqjNEQwJT9Z6UMRV9GLWCErPveAG1VmfgcBymp?filename=0-GRUMPY_CAT.json
-https://ipfs.io/ipfs/QmdnRfUR4RhJTvrGwqZuLrVn4biFDU7PwjowhAWbziMsSM?filename=cat.jpeg
 https://testnets.opensea.io/
-https://testnets.opensea.io/assets/0x00C536e656382d80f314895dF7b772b2557F40Ef/0
 
 
 Simple NFT without setting a tokenURI
--- scripts/advanced_collectible/deploy_advanced.py
+++ scripts/advanced_collectible/deploy_advanced.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
+import os
 from brownie import AdvancedCollectible, accounts, network, config
 from scripts.helpful_scripts import fund_with_link
 
 def main():
     dev = accounts.add(config["wallets"]["from_key"])
     print(network.show_active())
-    # publish_source = True if os.getenv("ETHERSCAN_TOKEN") else False # Currently having an issue with this
-    publish_source = False
+    publish_source = True if os.getenv("ETHERSCAN_TOKEN") else False # Currently having an issue with this
+    # publish_source = False
     advanced_collectible = AdvancedCollectible.deploy(
         config["networks"][network.show_active()]["vrf_coordinator"],
         config["networks"][network.show_active()]["link_token"],
